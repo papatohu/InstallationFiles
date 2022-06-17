7bdb5add6d10b489f5e6874f1be1e719f8fee0a1
P3lina <85225293+P3lina@users.noreply.github.com>
2022-06-17 10:58:10 +0200

    Add files via upload

---
+++ Frontend/assets/files/userUtils.ts
@@ -0,0 +1,75 @@
+export class UserUtils {
+  static newUserBody = {
+    "weather": {
+      "position": {
+        "x": 3,
+        "y": 5
+      }
+    },
+    "nasa": {
+      "position": {
+        "x": 3,
+        "y": 0
+      }
+    },
+    "cartoon": {
+      "position": {
+        "x": 6,
+        "y": 0
+      }
+    },
+    "maps": {
+      "position": {
+        "x": 9,
+        "y": 0
+      },
+      "mapsConfiguration": {
+        "origin": "Halle",
+        "destination": "Pforzheim",
+        "mode": "driving",
+        "avoid": [
+          "tolls",
+          "ferries"
+        ],
+        "measurements": "metric"
+      }
+    },
+    "public_transport": {
+      "position": {
+        "x": 0,
+        "y": 0
+      },
+      "origin": "Berlin"
+    },
+    "stocks": {
+      "position": {
+        "x": 3,
+        "y": 4
+      }
+    },
+    "text_of_the_day": {
+      "position": {
+        "x": 6,
+        "y": 4
+      }
+    },
+    "daily-news": {
+      "position": {
+        "x": 9,
+        "y": 4
+      }
+    },
+    "chuck": {
+      "position": {
+        "x": 6,
+        "y": 5
+      }
+    },
+    "tronaldDump": {
+      "position": {
+        "x": 9,
+        "y": 5
+      }
+    }
+  }
+}
